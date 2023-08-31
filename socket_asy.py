326f1fc3f9db467dddd121227bdd65abcf0fd9e7
fenix <fenix217grom@gmail.com>
2023-08-31 14:12:13 +0300

    рабочий TCP, UDP_multicast сокет, максимально упрощен интерфейс. Нужно добавить обработку ошибок сокет

--- socket_asy.py
+++ socket_asy.py
@@ -19,87 +19,70 @@ class typeSocket(Enum):
 
 class Socket_asy():
 
+    async def recv(self, loop, s):
+        try:
+            data = loop.sock_recv(s, 1024)
+            result = await data
+            return result
+        except socket.error as msg:
+            print(f'исключение {msg}')
+        except Exception as msg:
+            print(f'Аларм {msg}')
+
+    async def write(self, loop, s, byte: bytes):
+        return await loop.sock_sendall(s, byte)
+
     async def new_connect(self, type_socket: typeSocket,
-                          ip, port):
-        '''TCP
-                ip - адрес сервера \n
-                port - порт сервера \n
-            UDP
-                ip - адрес сервера \n
-                port - порт сервера \n
-            UDP multicast
-                ip - адрес сетевого интерфейса (своего) \n
-                port - порт мультикаст рассылки \n
-                m_group - адрес мультикаст рассылки (227.0.1.1)
-            '''
+                          ip, port, m_group=None):
+        '''UDP multicast \n
+            ip - адрес сетевого интерфейса (своего) \n
+            port - порт мультикаст рассылки \n
+            m_group - адрес мультикаст рассылки (227.0.1.1)
+        '''
+
         s = socket.socket(*type_socket.value)
         s.bind(('', port))
         s.setblocking(0)
         loop = asyncio.get_event_loop()
-        await loop.sock_connect(s, (ip, port))
+        if type_socket == typeSocket.UDP_multicast:
+            merg = socket.inet_aton(m_group) + socket.inet_aton(ip)
+            s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, merg)
+        else:
+            await loop.sock_connect(s, (ip, port))
 
-        async def recv():
-            try:
-                data = loop.sock_recv(s, 1024)
-                result = await data
-                return result
-            except socket.error as msg:
-                print(f'исключение {msg}')
-            except Exception as msg:
-                print(f'Аларм {msg}')
+        async def wrapper_recv():
+            return await self.recv(loop, s)
 
-        async def write(byte: bytes):
-            return await loop.sock_sendall(s, byte)
-        return recv, write
+        async def wrapper_write(comm: bytes):
+            return await self.write(loop, s, comm)
 
-    async def new_connect_m(self, type_socket: typeSocket,
-                            interface_ip, multicast_port, m_group):
-        '''UDP multicast \n
-                ip - адрес сетевого интерфейса (своего) \n
-                port - порт мультикаст рассылки \n
-                m_group - адрес мультикаст рассылки (227.0.1.1)
-
-            '''
-
-        s = socket.socket(*type_socket.value)
-        s.bind(('', multicast_port))
-        s.setblocking(0)
-        merg = socket.inet_aton(m_group) + socket.inet_aton(interface_ip)
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, merg)
-        loop = asyncio.get_event_loop()
-        loop.sock_connect(s, (m_group, multicast_port))
-
-        async def recv():
-            try:
-                data = loop.sock_recv(s, 1024)
-                result = await data
-                return result
-            except socket.error as msg:
-                print(f'исключение {msg}')
-            except Exception as msg:
-                print(f'Аларм {msg}')
-
-        async def write(byte: bytes):
-            return await loop.sock_sendall(s, byte)
-        return recv, write
+        return wrapper_recv, wrapper_write
 
 
-async def main():
+async def main(arg):
     sock = Socket_asy()
-    # reader, writer = await sock.new_connect_m(typeSocket.UDP_multicast,
-    #                                           '192.168.0.34',
-    #                                           39090, '227.0.1.1')
-    # while 1:
-    #     x = reader()
-    #     print(await x)
 
-    reader, writer = await sock.new_connect(typeSocket.TCP,
-                                            '192.168.1.115',
-                                            9760)
-    await writer(b'$t#')
-    await writer(b'$t#')
-    while 1:
-        x = reader()
-        print(await x)
+    if arg == 3:
+        reader, writer = await sock.new_connect(typeSocket.UDP_multicast,
+                                                '192.168.0.34',
+                                                39090, '227.0.1.1')
+        while 1:
+            x = await reader()
+            print(x)
 
-asyncio.run(main())
+    if arg == 1:
+        reader, writer = await sock.new_connect(typeSocket.TCP,
+                                                '192.168.1.115',
+                                                9760)
+        await writer(b'$t#')
+        await writer(b'$t#')
+        while 1:
+            x = await reader()
+            print(x)
+
+TCP = 1
+UDP = 2
+UDP_multicast = 3
+
+if __name__ == '__main__':
+    asyncio.run(main(UDP_multicast))
