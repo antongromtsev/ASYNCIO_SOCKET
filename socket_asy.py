3788fe2f6696ee7706d3f30d34fcca07c070af2c
fenix <fenix217grom@gmail.com>
2023-09-01 17:17:00 +0300

    Убрал все диалоги м пользователем

--- socket_asy.py
+++ socket_asy.py
@@ -1,20 +1,14 @@
 import asyncio
 import socket
+import time
 from enum import Enum
 from datetime import datetime as dt
-# from socket import *
-# import Liter
 
 
 class typeSocket(Enum):
     TCP = (socket.AF_INET, socket.SOCK_STREAM)
     UDP = (socket.AF_INET, socket.SOCK_DGRAM)
     UDP_multicast = (socket.AF_INET, socket.SOCK_DGRAM)
-
-# type_socket = {'TCP': (socket.AF_INET, socket.SOCK_STREAM),
-#                'UDP': (socket.AF_INET, socket.SOCK_DGRAM),
-#                'UDP_multicast': (socket.AF_INET, socket.SOCK_DGRAM),
-# }
 
 
 class Socket_asy():
@@ -26,14 +20,27 @@ class Socket_asy():
             return result
         except socket.error as msg:
             print(f'исключение {msg}')
+            task = asyncio.current_task()
+            task.cancel()
         except Exception as msg:
             print(f'Аларм {msg}')
+            task = asyncio.current_task()
+            task.cancel()
 
     async def write(self, loop, s, byte: bytes):
-        return await loop.sock_sendall(s, byte)
+        try:
+            await loop.sock_sendall(s, byte)
+        except socket.error as msg:
+            print(f'исключение {msg}')
+            task = asyncio.current_task()
+            task.cancel()
+        except Exception as msg:
+            print(f'Аларм {msg}')
+            task = asyncio.current_task()
+            task.cancel()
 
     async def new_connect(self, type_socket: typeSocket,
-                          ip: str, port: int, m_group: str = None):
+                          ip: str, port: int, ip_interface: str = None):
         '''
         TCP/UDP \n
             ip - адрес сервера \n
@@ -59,13 +66,46 @@ class Socket_asy():
         s.setblocking(0)
         loop = asyncio.get_event_loop()
         if type_socket == typeSocket.UDP_multicast:
-            merg = socket.inet_aton(m_group) + socket.inet_aton(ip)
+            merg = socket.inet_aton(ip) + socket.inet_aton(ip_interface)
             s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, merg)
         else:
-            await loop.sock_connect(s, (ip, port))
+            i = 0
+            while 1:
+                try:
+                    await asyncio.wait_for(loop.sock_connect(s, (ip, port)), 5)
+                except TimeoutError:
+                    i += 1
+                    if i > 10:
+                        print(f'Не удалось подключиться к серверу ip: {ip}. '
+                              f'Проверьте подключение к сети')
+                        task = asyncio.current_task()
+                        task.cancel()
+                        break
+                    print(f'Нет ответа от сервера {ip}: попыток {i}')
+                    continue
+                break
 
         async def wrapper_recv():
-            return await self.recv(loop, s)
+            i = 0
+            start = None
+            while 1:
+                try:
+                    data = await asyncio.wait_for(self.recv(loop, s), 6)
+                except TimeoutError:
+                    i += 1
+                    if not start:
+                        start = time.time()
+                    if i > 10:
+                        stop = time.time()
+                        print(f'Нет данных от сервера {ip} в течении '
+                              f'{stop - start} сек.\n'
+                              f'Соеденение разорвано!')
+                        task = asyncio.current_task()
+                        task.cancel()
+                        break
+                    print(f'Нет данных от сервера {ip}: попыток {i}')
+                    continue
+                return data
 
         async def wrapper_write(comm: bytes):
             return await self.write(loop, s, comm)
@@ -74,47 +114,33 @@ class Socket_asy():
             await self.write(loop, s, comm)
             result = b''
             while verification not in result:
-                data = await self.recv(loop, s)
-                result += data
+                if data := await wrapper_recv():  # self.recv(loop, s)
+                    result += data
             return result
 
         return (wrapper_recv if type_socket == typeSocket.UDP_multicast
                 else wrapper_recv, wrapper_write, wrapper_write_recv)
 
 
-async def main(arg):
+async def main():
     sock = Socket_asy()
-    # arg = 3
-    # if arg == 3:
-    #     r, w, wr = await sock.new_connect(typeSocket.UDP_multicast,
-    #                                             '192.168.0.34',
-    #                                             39090, '227.0.1.1')
-    #     while 1:
-    #         x = await r()
-    #         print(x)
-    # arg = 1
-    # if arg == 1:
-    #     r, w, wr = await sock.new_connect(typeSocket.TCP,
-    #                                             '192.168.1.115',
-    #                                             9760)
-    #     # await w(b'$t#')
-    #     # await w(b'$t#')
-    #     # while 1:
-    #     #     x = await r()
-    #     #     print(x)
-    #     x = await wr(b'$t#', b'$t')
-    #     print(x)
-    r_m, _, _ = await sock.new_connect(typeSocket.UDP_multicast,
-                                       '192.168.0.34',
-                                       39090, '227.0.1.1')
-    _, w, wr = await sock.new_connect(typeSocket.TCP,
-                                      '192.168.1.115',
-                                      9760)
+    t1 = asyncio.create_task(sock.new_connect(typeSocket.UDP_multicast,
+                                              '227.0.1.1',
+                                              39090, '192.168.0.34'))
+    t2 = asyncio.create_task(sock.new_connect(typeSocket.TCP,
+                                              '192.168.1.115',
+                                              9760))
 
-    async def cycle(func, *args):
+    r_m, _, _ = await t1
+    r, w, wr = await t2
+
+    async def cycle(func: asyncio.Future, *args):
         while 1:
-            print(dt.now(), await func(*args))
-            await asyncio.sleep(5)
+            try:
+                print(dt.now(), await func(*args))
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                break
 
     t1 = asyncio.create_task(cycle(r_m))
     t2 = asyncio.create_task(cycle(wr, b'$t#', b'$t'))
@@ -122,9 +148,5 @@ async def main(arg):
     await t2
 
 
-TCP = 1
-UDP = 2
-UDP_multicast = 3
-
 if __name__ == '__main__':
-    asyncio.run(main(UDP_multicast))
+    asyncio.run(main())
